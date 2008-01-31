dd0d83033364d286e760f6323b7dc78b22f29de1
nickseigal <nickseigal@8f7895f4-5222-0410-bd83-a8d9bae5c72b>
2008-01-31 22:07:21 +0000

    Added a folder remotely

---
+++ branches/LaneDotNet/ORMAP_TaxlotEditing/CombineTaxlots.vb
@@ -0,0 +1,106 @@
+Imports System.Runtime.InteropServices
+Imports System.Drawing
+Imports ESRI.ArcGIS.ADF.BaseClasses
+Imports ESRI.ArcGIS.ADF.CATIDs
+Imports ESRI.ArcGIS.Framework
+Imports ESRI.ArcGIS.ArcMapUI
+
+<ComClass(CombineTaxlots.ClassId, CombineTaxlots.InterfaceId, CombineTaxlots.EventsId), _
+ ProgId("ORMAPTaxlotEditing.CombineTaxlots")> _
+Public NotInheritable Class CombineTaxlots
+    Inherits BaseCommand
+
+#Region "COM GUIDs"
+    ' These  GUIDs provide the COM identity for this class 
+    ' and its COM interfaces. If you change them, existing 
+    ' clients will no longer be able to access the class.
+    Public Const ClassId As String = "cd9a4052-836a-42a1-bd09-b3bee8dee1a9"
+    Public Const InterfaceId As String = "3429b024-92e6-4e59-b2fb-720efce1a98c"
+    Public Const EventsId As String = "c612e825-3371-407d-8fb9-f478981a22dc"
+#End Region
+
+#Region "COM Registration Function(s)"
+    <ComRegisterFunction(), ComVisibleAttribute(False)> _
+    Public Shared Sub RegisterFunction(ByVal registerType As Type)
+        ' Required for ArcGIS Component Category Registrar support
+        ArcGISCategoryRegistration(registerType)
+
+        'Add any COM registration code after the ArcGISCategoryRegistration() call
+
+    End Sub
+
+    <ComUnregisterFunction(), ComVisibleAttribute(False)> _
+    Public Shared Sub UnregisterFunction(ByVal registerType As Type)
+        ' Required for ArcGIS Component Category Registrar support
+        ArcGISCategoryUnregistration(registerType)
+
+        'Add any COM unregistration code after the ArcGISCategoryUnregistration() call
+
+    End Sub
+
+#Region "ArcGIS Component Category Registrar generated code"
+    Private Shared Sub ArcGISCategoryRegistration(ByVal registerType As Type)
+        Dim regKey As String = String.Format("HKEY_CLASSES_ROOT\CLSID\{{{0}}}", registerType.GUID)
+        MxCommands.Register(regKey)
+
+    End Sub
+    Private Shared Sub ArcGISCategoryUnregistration(ByVal registerType As Type)
+        Dim regKey As String = String.Format("HKEY_CLASSES_ROOT\CLSID\{{{0}}}", registerType.GUID)
+        MxCommands.Unregister(regKey)
+
+    End Sub
+
+#End Region
+#End Region
+
+
+    Private m_application As IApplication
+
+    ' A creatable COM class must have a Public Sub New() 
+    ' with no parameters, otherwise, the class will not be 
+    ' registered in the COM registry and cannot be created 
+    ' via CreateObject.
+    Public Sub New()
+        MyBase.New()
+
+        ' TODO: Define values for the public properties
+        MyBase.m_category = "ORMAP (.NET)"  'localizable text 
+        MyBase.m_caption = "Combine Selected Taxlots"   'localizable text 
+        MyBase.m_message = "This tool combines taxlots in the Taxlot polygon feature class."   'localizable text 
+        MyBase.m_toolTip = "Combine selected taxlots" 'localizable text 
+        MyBase.m_name = "combineTaxlots"  'unique id, non-localizable (e.g. "MyCategory_ArcMapCommand")
+
+        'Try
+        '    'TODO: change bitmap name if necessary
+        '    Dim bitmapResourceName As String = Me.GetType().Name + ".bmp"
+        '    MyBase.m_bitmap = New Bitmap(Me.GetType(), bitmapResourceName)
+        'Catch ex As Exception
+        '    System.Diagnostics.Trace.WriteLine(ex.Message, "Invalid Bitmap")
+        'End Try
+
+
+    End Sub
+
+
+    Public Overrides Sub OnCreate(ByVal hook As Object)
+        If Not hook Is Nothing Then
+            m_application = CType(hook, IApplication)
+
+            'Disable if it is not ArcMap
+            If TypeOf hook Is IMxApplication Then
+                MyBase.m_enabled = True
+            Else
+                MyBase.m_enabled = False
+            End If
+        End If
+
+        ' TODO:  Add other initialization code
+    End Sub
+
+    Public Overrides Sub OnClick()
+        'TODO: Add combineTaxlots.OnClick implementation
+    End Sub
+End Class
+
+
+
---
+++ branches/LaneDotNet/ORMAP_TaxlotEditing/LocateFeature.vb
@@ -0,0 +1,107 @@
+Imports System.Runtime.InteropServices
+Imports System.Drawing
+Imports ESRI.ArcGIS.ADF.BaseClasses
+Imports ESRI.ArcGIS.ADF.CATIDs
+Imports ESRI.ArcGIS.Framework
+Imports ESRI.ArcGIS.ArcMapUI
+
+<ComClass(LocateFeature.ClassId, LocateFeature.InterfaceId, LocateFeature.EventsId), _
+ ProgId("ORMAPTaxlotEditing.LocateFeature")> _
+Public NotInheritable Class LocateFeature
+    Inherits BaseCommand
+
+#Region "COM GUIDs"
+    ' These  GUIDs provide the COM identity for this class 
+    ' and its COM interfaces. If you change them, existing 
+    ' clients will no longer be able to access the class.
+    Public Const ClassId As String = "ecace054-c346-4be5-a980-8d81ede68fcb"
+    Public Const InterfaceId As String = "effc83d3-a320-460c-9b1e-7f48a4b282d8"
+    Public Const EventsId As String = "591a6993-fcb1-4c0c-879f-5488e0c88312"
+#End Region
+
+#Region "COM Registration Function(s)"
+    <ComRegisterFunction(), ComVisibleAttribute(False)> _
+    Public Shared Sub RegisterFunction(ByVal registerType As Type)
+        ' Required for ArcGIS Component Category Registrar support
+        ArcGISCategoryRegistration(registerType)
+
+        'Add any COM registration code after the ArcGISCategoryRegistration() call
+
+    End Sub
+
+    <ComUnregisterFunction(), ComVisibleAttribute(False)> _
+    Public Shared Sub UnregisterFunction(ByVal registerType As Type)
+        ' Required for ArcGIS Component Category Registrar support
+        ArcGISCategoryUnregistration(registerType)
+
+        'Add any COM unregistration code after the ArcGISCategoryUnregistration() call
+
+    End Sub
+
+#Region "ArcGIS Component Category Registrar generated code"
+    Private Shared Sub ArcGISCategoryRegistration(ByVal registerType As Type)
+        Dim regKey As String = String.Format("HKEY_CLASSES_ROOT\CLSID\{{{0}}}", registerType.GUID)
+        MxCommands.Register(regKey)
+
+    End Sub
+    Private Shared Sub ArcGISCategoryUnregistration(ByVal registerType As Type)
+        Dim regKey As String = String.Format("HKEY_CLASSES_ROOT\CLSID\{{{0}}}", registerType.GUID)
+        MxCommands.Unregister(regKey)
+
+    End Sub
+
+#End Region
+#End Region
+
+
+    Private m_application As IApplication
+
+    ' A creatable COM class must have a Public Sub New() 
+    ' with no parameters, otherwise, the class will not be 
+    ' registered in the COM registry and cannot be created 
+    ' via CreateObject.
+    Public Sub New()
+        MyBase.New()
+
+        ' TODO: Define values for the public properties
+        MyBase.m_category = "ORMAP (.NET)"  'localizable text 
+        MyBase.m_caption = "Locate Taxlot/Mapindex"   'localizable text 
+        MyBase.m_message = "Use this tool to locate a Taxlot/Mapindex"   'localizable text 
+        MyBase.m_toolTip = "Locate Taxlot/Mapindex" 'localizable text 
+        MyBase.m_name = "locateFeature"  'unique id, non-localizable (e.g. "MyCategory_ArcMapCommand")
+
+        Try
+            'TODO: change bitmap name if necessary
+            Dim bitmapResourceName As String = Me.GetType().Name + ".bmp"
+            MyBase.m_bitmap = New Bitmap(Me.GetType(), bitmapResourceName)
+        Catch ex As Exception
+            System.Diagnostics.Trace.WriteLine(ex.Message, "Invalid Bitmap")
+        End Try
+
+
+
+    End Sub
+
+
+    Public Overrides Sub OnCreate(ByVal hook As Object)
+        If Not hook Is Nothing Then
+            m_application = CType(hook, IApplication)
+
+            'Disable if it is not ArcMap
+            If TypeOf hook Is IMxApplication Then
+                MyBase.m_enabled = True
+            Else
+                MyBase.m_enabled = False
+            End If
+        End If
+
+        ' TODO:  Add other initialization code
+    End Sub
+
+    Public Overrides Sub OnClick()
+        'TODO: Add locateFeature.OnClick implementation
+    End Sub
+End Class
+
+
+
