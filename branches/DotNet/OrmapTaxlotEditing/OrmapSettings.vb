8dab57f1cbda5225608b67d41029582c827c2c1e
telesforos <telesforos@8f7895f4-5222-0410-bd83-a8d9bae5c72b>
2008-02-26 00:41:30 +0000

    Added SVN Keyword region to source files and keywords to properties.

--- branches/DotNet/OrmapTaxlotEditing/OrmapSettings.vb
+++ branches/DotNet/OrmapTaxlotEditing/OrmapSettings.vb
@@ -28,6 +28,11 @@
 ' Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 
 #End Region
+#Region "Subversion Keyword expansion"
+'Tag for this file: $Name$
+'SCC revision number: $Revision:$
+'Date of Last Change: $Date:$
+#End Region
 
 ' TODO: Implement this class and move code here from the form so as to seperate
 ' the UI and business logic layers.
